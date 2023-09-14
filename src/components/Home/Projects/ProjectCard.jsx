20af7cc481f1517edebe2870d247238371aab60a
Wai Ian Vong <valenvwi@gmail.com>
2023-09-14 10:14:32 +0200

    delete extra div and fix margin

--- src/components/Home/Projects/ProjectCard.jsx
+++ src/components/Home/Projects/ProjectCard.jsx
@@ -78,109 +78,107 @@ export const ProjectCard = () => {
   window.addEventListener("scroll", showCard);
 
   return (
-    <div>
-      <div className="d-lg-block">
-        <div className="row project-card left border shadow-lg">
-          <div className="col-sm-12 col-md-8 container d-flex justify-content-center align-items-center">
-            <div className="ml-2">
-              <h1>InstaExplore</h1>
-              <p className="lead">
-                InstaExplore is a mobile application designed to help newcomers
-                discover their local area and connect with people in their
-                vicinity. Inspired by popular social media platforms like
-                Instagram, InstaExplore combines the power of geolocation and
-                user-generated content to create an engaging and interactive
-                experience.
-              </p>
-              <h5>Tech-Stack</h5>
-              <div className="row">
-                <div className="col-sm-12 col-lg-8 mb-3">
-                  {firstIcons.map((icon) => (
-                    <ProjectTechIcons key={Math.random()} icon={icon} />
-                  ))}
-                </div>
-                <div className="col-sm-12 col-lg-4 my-3 text-center">
-                  <ProjectButton
-                    website={buttons[0].href}
-                    title={buttons[0].title}
-                  />
-                </div>
+    <div className="d-lg-block">
+      <div className="row project-card left border shadow-lg">
+        <div className="col-sm-12 col-md-8 container d-flex justify-content-center align-items-center">
+          <div className="ml-2">
+            <h1>InstaExplore</h1>
+            <p className="lead">
+              InstaExplore is a mobile application designed to help newcomers
+              discover their local area and connect with people in their
+              vicinity. Inspired by popular social media platforms like
+              Instagram, InstaExplore combines the power of geolocation and
+              user-generated content to create an engaging and interactive
+              experience.
+            </p>
+            <h5>Tech-Stack</h5>
+            <div className="row">
+              <div className="col-sm-12 col-lg-8 mb-3">
+                {firstIcons.map((icon) => (
+                  <ProjectTechIcons key={Math.random()} icon={icon} />
+                ))}
               </div>
-            </div>
-          </div>
-
-          <div className="d-none d-lg-block col-lg-4 text-center py-2">
-            <ProjectCardImage href={images[0].href} title={images[0].title} />
-          </div>
-        </div>
-
-        <div className="row project-card right border shadow-lg">
-          <div className="d-none d-lg-block col-lg-4 text-center py-2">
-            <img src={starry} alt="InstaExplore" className="card-image"></img>
-          </div>
-
-          <div className="col-sm-12 col-md-8 container d-flex justify-content-center align-items-center">
-            <div className="ml-2">
-              <h1>Starry Accessory</h1>
-              <p className="lead">
-                Starry Accessory is an online earrings shop that offers a
-                straightforward shopping experience. Visitors can freely browse
-                a variety of products. Upon logging in, users gain the ability
-                to add items to their cart, proceed with the checkout process,
-                and conveniently track their order history.
-              </p>
-              <h5>Tech-Stack</h5>
-              <div className="row">
-                <div className="col-sm-12 col-lg-8 mb-3">
-                  {secondIcons.map((icon) => (
-                    <ProjectTechIcons key={Math.random()} icon={icon} />
-                  ))}
-                </div>
-                <div className="col-sm-12 col-lg-4 my-3 text-center">
-                  <ProjectButton
-                    website={buttons[1].href}
-                    title={buttons[1].title}
-                  />
-                </div>
+              <div className="col-sm-12 col-lg-4 my-3 text-center">
+                <ProjectButton
+                  website={buttons[0].href}
+                  title={buttons[0].title}
+                />
               </div>
             </div>
           </div>
         </div>
 
-        <div className="row project-card left border shadow-lg">
-          <div className="col-sm-12 col-md-8 container d-flex justify-content-center align-items-center">
-            <div className="ml-2">
-              <h1>QuickDrop</h1>
-              <p className="lead">
-                Sending or receiving packages can be a hassle in today's
-                fast-paced world. With just a few taps on your smartphone, you
-                can send or receive packages on time and with complete
-                transparency. Originally, this was a two-week student project
-                made by Wai Ian Vong, Micaela Frigola, Mayavi Steiner and
-                Corinna Grundmeier. I have added additional pages to complete
-                the driver's journey, edited validations for orders, and add-ons
-                to improve the user's journey.
-              </p>
-              <h5>Tech-Stack</h5>
-              <div className="row">
-                <div className="col-sm-12 col-lg-8 mb-3">
-                  {thirdIcons.map((icon) => (
-                    <ProjectTechIcons key={Math.random()} icon={icon} />
-                  ))}
-                </div>
-                <div className="col-sm-12 col-lg-4 my-3 text-center">
-                  <ProjectButton
-                    website={buttons[2].href}
-                    title={buttons[2].title}
-                  />
-                </div>
+        <div className="d-none d-lg-block col-lg-4 text-center py-2">
+          <ProjectCardImage href={images[0].href} title={images[0].title} />
+        </div>
+      </div>
+
+      <div className="row project-card right border shadow-lg">
+        <div className="d-none d-lg-block col-lg-4 text-center py-2">
+          <img src={starry} alt="InstaExplore" className="card-image"></img>
+        </div>
+
+        <div className="col-sm-12 col-md-8 container d-flex justify-content-center align-items-center">
+          <div className="ml-2">
+            <h1>Starry Accessory</h1>
+            <p className="lead">
+              Starry Accessory is an online earrings shop that offers a
+              straightforward shopping experience. Visitors can freely browse a
+              variety of products. Upon logging in, users gain the ability to
+              add items to their cart, proceed with the checkout process, and
+              conveniently track their order history.
+            </p>
+            <h5>Tech-Stack</h5>
+            <div className="row">
+              <div className="col-sm-12 col-lg-8 mb-3">
+                {secondIcons.map((icon) => (
+                  <ProjectTechIcons key={Math.random()} icon={icon} />
+                ))}
+              </div>
+              <div className="col-sm-12 col-lg-4 my-3 text-center">
+                <ProjectButton
+                  website={buttons[1].href}
+                  title={buttons[1].title}
+                />
               </div>
             </div>
           </div>
+        </div>
+      </div>
 
-          <div className="d-none d-lg-block col-lg-4 text-center py-2">
-            <ProjectCardImage href={images[1].href} title={images[1].title} />
+      <div className="row project-card left border shadow-lg">
+        <div className="col-sm-12 col-md-8 container d-flex justify-content-center align-items-center">
+          <div className="ml-2">
+            <h1>QuickDrop</h1>
+            <p className="lead">
+              Sending or receiving packages can be a hassle in today's
+              fast-paced world. With just a few taps on your smartphone, you can
+              send or receive packages on time and with complete transparency.
+              Originally, this was a two-week student project made by Wai Ian
+              Vong, Micaela Frigola, Mayavi Steiner and Corinna Grundmeier. I
+              have added additional pages to complete the driver's journey,
+              edited validations for orders, and add-ons to improve the user's
+              journey.
+            </p>
+            <h5>Tech-Stack</h5>
+            <div className="row">
+              <div className="col-sm-12 col-lg-8 mb-3">
+                {thirdIcons.map((icon) => (
+                  <ProjectTechIcons key={Math.random()} icon={icon} />
+                ))}
+              </div>
+              <div className="col-sm-12 col-lg-4 my-3 text-center">
+                <ProjectButton
+                  website={buttons[2].href}
+                  title={buttons[2].title}
+                />
+              </div>
+            </div>
           </div>
+        </div>
+
+        <div className="d-none d-lg-block col-lg-4 text-center py-2">
+          <ProjectCardImage href={images[1].href} title={images[1].title} />
         </div>
       </div>
     </div>
