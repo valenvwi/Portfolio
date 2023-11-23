a708c365fdf0885432de461c8fb52a6deb9a2b9e
Wai Ian Vong <valenvwi@gmail.com>
2023-11-23 22:35:46 +0100

    Edit aboutMeContent

--- src/App/Home/AboutMe/AboutMeContent.jsx
+++ src/App/Home/AboutMe/AboutMeContent.jsx
@@ -1,8 +1,8 @@
 export const AboutMeContent = () => {
   return (
     <>
-      <h5>🍎 Originally from Macau and move to Zurich in 2021</h5>
-      <h5> 🧑‍🎓 Completed a web development bootcamp in Le Wagon in June 2023</h5>
+      <h5>🍎 Originally from Macau and moved to Zurich in 2021</h5>
+      <h5> 🧑‍🎓 Completed a web development bootcamp at Le Wagon in June 2023</h5>
       <h5>
         💻 I enjoy working on personal projects and finding interesting
         solutions
